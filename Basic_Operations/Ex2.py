0521439f616d5f7ad6aaba61ff07b1624095ec70
Anthony Doak <anthony.doak2@gmail.com>
2024-02-28 02:50:05 +0000

    commit Basic_Operatons

---
+++ Basic_Operations/Ex2.py
@@ -0,0 +1,12 @@
+my_num = 4936
+ones = my_num % 10
+print(ones)
+my_num = my_num // 10
+tens = my_num % 10
+print(tens)
+my_num = my_num // 10
+hundreds = my_num % 10
+print(hundreds)
+my_num = my_num // 10
+thousands = my_num % 10
+print(thousands)
